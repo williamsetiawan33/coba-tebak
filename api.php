619b01bc961dca52a5bbebdbbe526663ee55fd94
lilymalisa <williamsetiawan33@gmail.com>
2023-06-28 16:34:42 +0700

    new api design

--- api.php
+++ api.php
@@ -93,8 +93,8 @@ if (isset($_GET["req"])) {
     }
     // if a request is a specific question that is contributed by a player
     else if ($_GET["req"] == "questionPlayer") {
-        if (isset($_GET["player"])) {
-            $query = mysqli_query($conn, "SELECT * FROM questions WHERE playerId = " . '"' . $_GET["player"] . '"');
+        if (isset($_GET["player"]) && isset($_GET["level"])) {
+            $query = mysqli_query($conn, "SELECT * FROM questions WHERE playerId = " . '"' . $_GET["player"] . '" AND level = ' . $_GET["level"]);
             $result = [];
             while ($row = mysqli_fetch_assoc($query)) {
                 $result[] = $row;
@@ -220,6 +220,12 @@ if (isset($_GET["req"])) {
         } else {
             echo '{"message":"wrong request format"}';
         }
+    } else if ($_GET["req"] = "removeQuestion") {
+        if (isset($_GET["questionId"])) {
+            $query = mysqli_query($conn, "DELETE FROM questions WHERE questionId = " . '"' . $_GET["questionId"] . '"');
+        } else {
+            echo '{"message":"wrong request format"}';
+        }
     } else {
         echo '{"message":"wrong request format"}';
     }
@@ -283,6 +289,28 @@ if (isset($_POST["submit"]) && isset($_POST["name"]) && isset($_POST["signUp"]))
     mysqli_close($conn);
 }
 
+// create answer request
+if (isset($_POST["submit"]) && isset($_POST["createQuestion"])) {
+    if (isset($_POST["playerId"]) && isset($_POST["question"]) && isset($_POST["answer"]) && isset($_POST["hint"]) && isset($_POST["level"])) {
+        // algoritma random id
+        $n = 10;
+        $charArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        $randomQuestionId = "";
+        for ($i = 0; $i < $n; $i++) {
+            $index = rand(0, count($charArray) - 1);
+            $randomQuestionId .= $charArray[$index];
+        }
+        // echo $randomPlayerId;
+
+        $result = mysqli_query($conn, "INSERT INTO questions VALUES(" . '"' . $randomQuestionId . '","' . $_POST["playerId"] . '","' . $_POST["question"] . '","' . $_POST["answer"] . '","' . $_POST["hint"] . '",' . $_POST["level"] . ')');
+
+        if ($result == true) {
+            echo '{"message":"succes"}';
+        } else if ($result == false) {
+            echo '{"message":"failed"}';
+        }
+    }
+}
 
 function randomPlayerId()
 {
